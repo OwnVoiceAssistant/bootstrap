cb71af4a590e16aca4c4c35bf7c1d8e3f3c16775
Charles Rao <qrao1977@gmail.com>
2019-06-23 04:38:12 +0100

    Add BT admin

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# debian-install-bootstrap
+# Bootstrap for OwnVoiceAssistant
+
+To run manual build of OwnVoiceAssistant, follow the steps:
+
+1. cd /home/pi
+2. wget https://raw.githubusercontent.com/OwnVoiceAssistant/bootstrap/master/setup.sh
+3. bash setup.sh
--- setup.sh
+++ setup.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-sudo touch /boot/ssh
+sudo systemctl enable ssh
 
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install -y build-essentials git screen
+sudo apt-get install -y build-essential git screen
 
+##########################################################################
 # Setup Bluetooth admin
 cd $HOME
 git clone https://github.com/OwnVoiceAssistant/raspi-btadmin.git
+cd raspi-btadmin && make setup && make install
+
+##########################################################################
+# Put frequently updated files into memory to increase the lifespan of the sdcard
+sudo tee -a /etc/fstab > /dev/null << EOL
+
+# Put frequently updated files into memory to increase the lifespan of the sdcard
+tmpfs    /tmp    tmpfs    defaults,noatime,nosuid,size=10m    0 0
+tmpfs    /var/tmp    tmpfs    defaults,noatime,nosuid,size=5m    0 0
+tmpfs    /var/log    tmpfs    defaults,noatime,nosuid,mode=0755,size=10m    0 0
+
+EOL
+
+##########################################################################
+cd $HOME
 
 sudo reboot
